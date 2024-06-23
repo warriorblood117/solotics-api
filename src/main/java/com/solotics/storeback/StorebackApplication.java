1a31af092b173aefa9af51cdf5d96cb4ac652b84
warriorblood117 <warriorblood117@gmail.com>
2024-06-23 15:30:17 -0500

    init project

---
+++ src/main/java/com/solotics/storeback/StorebackApplication.java
@@ -0,0 +1,13 @@
+package com.solotics.storeback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorebackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorebackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solotics/storeback/StorebackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solotics.storeback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorebackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
